aeeab111059d160c592f937aaf4f255c8eb06696
Drew Steen <andrew.decker.steen@gmail.com>
2019-10-24 14:49:49 -0400

    miscDemos loads a file and makes a plot

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Notes on growthcurves_frontend
 
 This is the nascent streamlit app that will act as the frontend of code by David Talmy and Spiro Papoulis to fit models to bacterial growth curves.
+
+To run: I'm using a virtualenv managed by conda with python 3.7.3. 
+
+Before running, I need to run `conda activate growthcurves_frontend`. 
+
+To install modules, I need to use `~/anaconda/envs/growthcurves_frontend/bin/pip install`
---
+++ miscDemos.py
@@ -0,0 +1,38 @@
+# Throwing together lots of crap from streamlit demos
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy as cp  # Wild that you need a package to copy objects
+
+# Define read_and_cache_csv, which kinda is like a function
+read_and_cache_csv = st.cache(pd.read_csv)
+
+st.title('My second app')
+
+st.write("Here's our first attempt at using data to create a table:")
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [9, 21, 29, 41]
+})
+
+newDf = cp.copy(df)
+st.write(newDf)
+
+st.write("Now for my own data")
+myData = read_and_cache_csv("data/test.csv")
+myData['Col2noisy'] = myData['Col2'] + np.random.normal(size=5)
+st.write(myData)
+
+# Make a chart
+st.write("Here's a plot")
+# myPlot = myData.plot.scatter(x='Col1', y='Col2')
+plt.scatter(myData.Col1, myData.Col2)
+plt.scatter(myData.Col1, myData.Col2noisy)
+st.pyplot()
+# st.line_chart(myData)
+
+# Button to redo the noisy data
+st.button("Rerun", key="rerunButton")
