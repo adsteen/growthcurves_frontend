94689fad74e723cf5c321afaf747ae3bcf2ab252
Drew Steen <andrew.decker.steen@gmail.com>
2019-10-24 15:06:49 -0400

    dunno

--- growthcurves.py
+++ growthcurves.py
@@ -2,33 +2,45 @@
 # Currently copied from streamlit plotting example
 
 import streamlit as st
-import time
+import pandas as pd
 import numpy as np
 
+# Leftover; I'm not really sure what these do
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
 
-# Janky file selector based on https://discuss.streamlit.io/t/upload-files-to-streamlit-app/80/2
+# Title the app
+st.title("Frontend for growth curves modeling software")
+
+st.text("my butt")
+
+# Janky file selector based on
+# https://discuss.streamlit.io/t/upload-files-to-streamlit-app/80/2
 filename = st.text_input('Enter a file path:', value="data/test.csv")
+
 try:
     with open(filename) as input:
-        st.text(input.read())
-except FileNotFoundError:
+        # df = input.read()
+		st.text(input.read())
     st.error('File not found.')
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.001)
+# st.text(input.read(filename))
 
-progress_bar.empty()
+
+# Plot the data frame I've imported
+# myPlot=myData.plot.scatter(x='time', y='abundance')
+
+# for i in range(1, 101):
+#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+#     status_text.text("%i%% Complete" % i)
+#     chart.add_rows(new_rows)
+#     progress_bar.progress(i)
+#     last_rows = new_rows
+#     time.sleep(0.001)
+
+#progress_bar.empty()
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
-st.button("Re-run")
+# st.button("Re-run")
