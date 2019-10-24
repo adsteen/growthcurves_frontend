67dfb0d49bf5b2a95d4c8d3aef5058afc34dce78
Drew Steen <andrew.decker.steen@gmail.com>
2019-10-24 13:12:13 -0400

    added working (but ugly) file picker

--- growthcurves.py
+++ growthcurves.py
@@ -10,13 +10,21 @@ status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
 
+# Janky file selector based on https://discuss.streamlit.io/t/upload-files-to-streamlit-app/80/2
+filename = st.text_input('Enter a file path:', value="data/test.csv")
+try:
+    with open(filename) as input:
+        st.text(input.read())
+except FileNotFoundError:
+    st.error('File not found.')
+
 for i in range(1, 101):
     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
     status_text.text("%i%% Complete" % i)
     chart.add_rows(new_rows)
     progress_bar.progress(i)
     last_rows = new_rows
-    time.sleep(0.05)
+    time.sleep(0.001)
 
 progress_bar.empty()
 
